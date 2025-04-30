ba161812e8d01acf343aa3591c621fd02f78e0a0
Muriloferbhs <murfeg@gmail.com>
2025-04-30 10:36:03 -0300

    Create BeeCrowd2413.java

---
+++ BeeCrowd2413.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		t *= 4;
+		
+		System.out.println(t);
+		
+		sc.close();
+	}
+}
