e5a4a85650d3144851f9cc178e749efae7226c58
Muriloferbhs <murfeg@gmail.com>
2025-05-06 08:35:30 -0300

    Create tentativa1837.java

---
+++ tentativa1837.java
@@ -0,0 +1,23 @@
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(a > 0 && b!=0){
+            int q = a / b;
+            int r = a % b;
+            
+            System.out.println(q);
+            System.out.println(r);
+        }
+        if 
+        
+        
+        
+        
+        sc.close();
+	}
+}
