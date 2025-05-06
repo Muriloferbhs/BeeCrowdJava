491996d91da16a1663c9275d61abf7d0d430e267
Muriloferbhs <murfeg@gmail.com>
2025-05-06 10:40:32 -0300

    Create tentativa2028.java

---
+++ tentativa2028.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+            
+            while(sc.hasNext()){
+                int n = nextInt();
+                
+                int cont = 1;
+                
+                if (n == 0){
+                    System.out.println("Caso " + cont + ": 1 numero");
+                    System.out.println("0");
+                }
+                
+                else{
+                    int numeros = 0;
+                    while 
+                    
+                }
+                
+                
+                cont++;
+            }
+        sc.close();
+	}
+}
